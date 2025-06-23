eb9124b0f96d75e4196a0f7ab9a28156f5553792
wenquansjtu <142790196+wenquansjtu@users.noreply.github.com>
2025-06-23 15:46:39 +0400

    Update langgraph.tsx

--- apps/web/src/components/icons/langgraph.tsx
+++ apps/web/src/components/icons/langgraph.tsx
@@ -8,6 +8,13 @@ export function LangGraphLogoSVG({
   className?: string;
 }) {
   return (
-     <br>
+      <svg
+      width={width}
+      height={height}
+      viewBox="0 0 98 51"
+      fill="none"Add commentMore actions
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    ></svg>
   );
 }
